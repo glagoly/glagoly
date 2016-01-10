4065685c7f22e72819fb2b86291df38f081ac9a3
Денис <denys.potapov@gmail.com>
2016-01-10 19:41:58 +0200

    delete button

--- src/assets/js/components/vote-form.js
+++ src/assets/js/components/vote-form.js
@@ -7,10 +7,26 @@ var voteForm = {
  
     setup: function() {
         $("#add-button").click(voteForm.add);
+        $(".delete-button").click(voteForm.delete);
     },
  
+    delete: function() {
+        var alt = $(this).parents("li[data-alternative-id]").first();
+        var deleted = $("#delete-alternative").clone();
+        MotionUI.animateOut(alt, 'fade-out ease-out', function () {
+            alt.after(deleted);
+            MotionUI.animateIn(deleted, 'fade-in ease-out');    
+        });
+    },
+
     add: function() {
         var form = $("#add-form");
+        var text = form.find('.text');
+        if (!text.val().trim()) {
+
+            return;
+        }
+
         var alt = $("#alternative-template").clone();
         alt.find(".vote").val(form.find('.vote').val());
         alt.find(".text").html(form.find('.text').val());
