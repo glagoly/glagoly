94593be9b61c8cf2d2464df60b4601b31def6729
denys <denys.potapov@gmail.com>
2017-10-25 11:21:09 +0300

    updadte saving votes

--- apps/web/src/edit.erl
+++ apps/web/src/edit.erl
@@ -6,7 +6,7 @@
 
 poll_id() -> wf:to_list(wf:q(<<"id">>)).
 
-poll_alts() -> kvs:entries(kvs:get(feed, {poll, poll_id()}), alt, undefined).
+poll_alts() -> kvs:entries(kvs:get(feed, {alts, poll_id()}), alt, undefined).
 
 alt(Alt, Vote) ->
 	[#li{body=[
@@ -36,7 +36,7 @@ main() ->
 		_ -> wf:state(status,404), "Poll not found" end.
 
 event(add_alt) ->
-	Alt = #alt{id=kvs:next_id(alt, 1), feed_id={poll, poll_id()}, text=wf:q(alt_text)},
+	Alt = #alt{id=kvs:next_id(alt, 1), feed_id={alts, poll_id()}, text=wf:q(alt_text)},
 	kvs:add(Alt),
 	wf:insert_bottom(alts, alt(Alt, wf:q(alt_vote)));
 
@@ -44,15 +44,15 @@ event(_) -> ok.
 
 prepare_prefs(Votes) -> 
 	% to pairs of ints
-	P1 = [{ filter:int(hd(tl(V)), -3, 7, 0), wf:to_integer(hd(V))} || V <- Votes],
+	P1 = [{ wf:to_integer(A), filter:int(V, -3, 7, 0)} || [A, V] <- Votes],
 	% remove not incorrect alt ids and zero prefs,
 	Alts = [Alt#alt.id || Alt <- poll_alts()],
-	lists:filter(fun({P, A}) -> (P /= 0) and lists:member(A, Alts) end, P1).
+	lists:filter(fun({A, V}) -> (V /= 0) and lists:member(A, Alts) end, P1).
 
 api_event(vote, Data, _) ->
 	{Props} = jsone:decode(list_to_binary(Data)),
 	Prefs = prepare_prefs(proplists:get_value(<<"votes">>, Props)),
 	Name = filter:string(proplists:get_value(<<"name">>, Props), 32, <<"anon">>),
-	Vote = #vote{id=kvs:next_id(vote, 1), feed_id={poll, poll_id()}, name=Name, prefs=some},
+	Vote = #vote{id=kvs:next_id(vote, 1), feed_id={votes, poll_id()}, name=Name, prefs=Prefs},
 	kvs:add(Vote),
 	wf:redirect("/result?id=" ++ wf:to_list(poll_id())).
