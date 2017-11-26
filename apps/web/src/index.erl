b05b7741e78461f875053062e3a14a513c3dd588
denys <denys.potapov@gmail.com>
2017-11-26 19:13:11 +0200

    fb login button

--- apps/web/src/index.erl
+++ apps/web/src/index.erl
@@ -5,13 +5,20 @@
 -include_lib("records.hrl").
 
 main() ->
+	wf:wire(#api{name=fb_login}),
 	#dtl{file="index", app=sample, bindings=[
 		{fb_app_id, wf:config(glagoly, fb_app_id)},
-		{create_button, #button{body="create poll",postback=create} 
-	}]}.
+		{create_button, #button{body="create poll",postback=create}},
+		{login_button, #button{body="login",onclick="onLoginClick();"}}
+	]}.
 
 event(create) -> 
 	Id = polls:create(usr:ensure()),
 	wf:redirect("/edit?id=" ++ wf:to_list(Id));
 
 event(_) -> ok.
+
+api_event(fb_login, Data, _) ->
+	Token = jsone:decode(list_to_binary(Data)),
+	wf:info(?MODULE,"Token: ~p~n",[Token]).
+
