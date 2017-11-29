b1e7a17c85aa144776deb56f0d1934ae271aa022
denys <denys.potapov@gmail.com>
2017-11-30 00:18:51 +0200

    better url

--- apps/vote_core/src/vote_core.erl
+++ apps/vote_core/src/vote_core.erl
@@ -63,8 +63,10 @@ rand_seq(Length, Seed) ->
 
 %% Create a UUID v4 (random) as a base64 string
 %% "+" are replaced with "-"
+%% "/" are replaced with "~"
 %% source avtobiff/erlang-uuid
 uuid() ->
     <<U0:32, U1:16, _:4, U2:12, _:2, U3:30, U4:32>> = crypto:strong_rand_bytes(16),
     U = base64:encode_to_string(<<U0:32, U1:16, 4:4, U2:12, 2#10:2, U3:30, U4:32>>),
-    lists:sublist(re:replace(U, "\\+", "-", [global, {return, list}]), 22).
+    P = [global, {return, list}],
+    lists:sublist(re:replace(re:replace(U, "\\/", "\\~", P), "\\+", "-", P), 22).
