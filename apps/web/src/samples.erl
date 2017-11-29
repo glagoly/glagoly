d66f891e36b42a1e0511ec0e77f7b8b2f620ad9c
denys <denys.potapov@gmail.com>
2017-11-30 00:10:38 +0200

    add sample names and alts

--- apps/web/src/samples.erl
+++ apps/web/src/samples.erl
@@ -5,13 +5,26 @@
 
 polls() -> [poll, schedule, list].
 
+names() -> [alice, bob, carol, dave, erin, frank].
+
 title(poll) -> "Fenster poll";
 title(schedule) -> "Weekend schedule";
 title(list) -> "Coctail list".
 
+alts(poll) -> [
+	{a, "Paint fenster in red"},
+	{a, "Paint fenster in blue"},
+	{a, "Remove fenster and make bicyle parking"},
+	{e, "Replace fenster with metall one"},
+	{f, "Leave fenster as is"}];
+alts(schedule) -> [];
+alts(list) -> [].
+
+populate_poll(Poll) ->
+	Id = "sample-" ++ wf:to_list(Poll),
+	kvs:put(#poll{id = Id, user = a, title = wf:to_list(title(Poll))}),
+	[kvs:add(#alt{id=kvs:next_id(alt, 1), user=U, feed_id={alts, Id}, text=T})
+		|| {U, T} <- alts(Poll)].
+
 populate() ->
-	[kvs:put(#poll{
-		id = "sample-" ++ wf:to_list(P),
-		user=1,
-		title = wf:to_list(title(P))}
-		) || P <- polls()].
+	[populate_poll(P)|| P <- polls()].
