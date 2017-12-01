dea01c4534f1c2820edc89473f058261ffd84dfc
denys <denys.potapov@gmail.com>
2017-12-01 23:33:06 +0200

    add ballot

--- apps/web/src/samples.erl
+++ apps/web/src/samples.erl
@@ -11,20 +11,32 @@ title(poll) -> "Fenster poll";
 title(schedule) -> "Weekend schedule";
 title(list) -> "Coctail list".
 
-alts(poll) -> [
+alts(_) -> [
 	{a, "Paint fenster in red"},
 	{a, "Paint fenster in blue"},
 	{a, "Remove fenster and make bicyle parking"},
 	{e, "Replace fenster with metall one"},
-	{f, "Leave fenster as is"}];
-alts(schedule) -> [];
-alts(list) -> [].
+	{f, "Leave fenster as is"}].
+% alts(schedule) -> [];
+% alts(list) -> [].
+
+ballots(_) -> [
+	{a, "Alice", [3, 1, 0, 0, 1]}
+].
 
 populate_poll(Poll) ->
 	Id = "sample-" ++ wf:to_list(Poll),
 	kvs:put(#poll{id = Id, user = a, title = wf:to_list(title(Poll))}),
-	[kvs:add(#alt{id=kvs:next_id(alt, 1), user=U, feed_id={alts, Id}, text=T})
-		|| {U, T} <- alts(Poll)].
+	Alts = lists:map(fun({U, T}) ->
+		A = kvs:next_id(alt, 1),
+		kvs:add(#alt{id=A, user=U, feed_id={alts, Id}, text=T}),
+		A
+	end, alts(Poll)),
+	lists:map(fun({U, N, B}) ->
+		V = kvs:next_id(vote, 1),
+		B2 = lists:zip(Alts, B),
+		kvs:add(#vote{id=V, feed_id={votes, Id}, user_poll={U, Id}, name=N, ballot=B2})
+	end, ballots(Poll)).
 
 populate() ->
 	[populate_poll(P)|| P <- polls()].
