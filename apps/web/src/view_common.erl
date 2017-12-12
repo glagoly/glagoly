84c911005c6454cdc99915f653502533206e54c3
denys <denys.potapov@gmail.com>
2017-12-12 23:23:27 +0200

    add topbar with logout

--- apps/web/src/view_common.erl
+++ apps/web/src/view_common.erl
@@ -3,6 +3,10 @@
 -include_lib("n2o/include/wf.hrl").
 -include_lib("nitro/include/nitro.hrl").
 
+top_bar() ->
+	#panel{class='top-bar', body=[
+		#link{body="logout", delegate=view_common, postback=logout}
+	]}.
 
 bindings() -> [
 		{fb_app_id, wf:config(web, fb_app_id)}
@@ -10,3 +14,9 @@ bindings() -> [
 
 page(Bindings) -> 
 	#dtl{file="_page", bindings=bindings() ++ Bindings}.
+
+event(logout) ->
+	usr:logout(),
+	wf:redirect("/");
+
+event(_) -> ok.
