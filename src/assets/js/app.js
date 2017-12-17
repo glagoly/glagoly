e40122c0c10238a4fa24268957e08f08e5bb7ef4
denys <denys.potapov@gmail.com>
2017-12-17 21:27:59 +0200

    clear add alt form

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -2,6 +2,11 @@ protos = [$client,$bert]; N2O_start();
 
 function closeHelp() {
 	qi('help-callout').style.display = 'none';
-}
+};
+
+function clearAltForm() {
+	qi('alt_text').value = '';
+	qi('alt_vote').value = '';
+};
 
 $(document).ready(VoteForm.init);
