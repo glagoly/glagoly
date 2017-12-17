0cb5f83579ffa4b4b246df2eeffd25f64be9e004
denys <denys.potapov@gmail.com>
2017-12-17 21:44:17 +0200

    update submit form

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,12 +1,28 @@
 protos = [$client,$bert]; N2O_start();
 
 function closeHelp() {
-	qi('help-callout').style.display = 'none';
+    qi('help-callout').style.display = 'none';
 };
 
 function clearAltForm() {
-	qi('alt_text').value = '';
-	qi('alt_vote').value = '';
+    qi('alt_text').value = '';
+    qi('alt_vote').value = '';
 };
 
-$(document).ready(VoteForm.init);
+function voteSubmit() {
+    var x = document.querySelectorAll("#alts input[id^=\"vote\"]");
+    var votes = [];
+    for (var i = 0; i < x.length; i++) {
+        votes.push([x[i].id.substring(4), x[i].value]);
+    };
+
+    vote({
+        title:  qi('title') ? qi('title').value : '',
+        name: qi('name').value,
+        votes: votes
+    });
+    console.log(votes);
+    return false;
+};
+
+
