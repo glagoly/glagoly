52f6e3fd845ae4f29a90263e81efd56da3df2d00
Denys Potapov <denys.potapov@gmail.com>
2020-10-26 22:10:01 +0200

    fix scss reload

--- gulpfile.js
+++ gulpfile.js
@@ -145,7 +145,7 @@ function reload(done) {
 // Watch for changes to static assets, pages, Sass, and JavaScript
 function watch() {
   gulp.watch(PATHS.assets, copy);
-  gulp.watch('src/assets/scss/**/*.scss').on('all', sass);
+  gulp.watch('src/assets/scss/**/*.scss').on('all', gulp.series(sass, browser.reload));
   gulp.watch('src/assets/js/**/*.js').on('all', gulp.series(javascript, browser.reload));
   gulp.watch('src/assets/img/**/*').on('all', gulp.series(images, browser.reload));
 }
