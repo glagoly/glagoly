f413981bf7ae9cb266b7947112a8fbdc50ca47e7
Denys Potapov <denys.potapov@gmail.com>
2022-05-18 23:54:23 +0300

    google analytycs

--- priv/static/js/app.js
+++ priv/static/js/app.js
@@ -80,3 +80,17 @@ function checkLoginState() {
       statusChangeCallback(response);
     });
 }
+
+/**
+* Google Analytics
+*/
+
+(function(e,t,n,i,s,a,c){e[n]=e[n]||function(){(e[n].q=e[n].q||[]).push(arguments)}
+;a=t.createElement(i);c=t.getElementsByTagName(i)[0];a.async=true;a.src=s
+;c.parentNode.insertBefore(a,c)
+})(window,document,"galite","script","https://cdn.jsdelivr.net/npm/ga-lite@2/dist/ga-lite.min.js");
+
+if (document.location.hostname !== 'localhost') {
+    galite('create', 'UA-111273395-1', 'auto');
+    galite('send', 'pageview');
+}
